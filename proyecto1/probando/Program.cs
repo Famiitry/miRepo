cefdcd52636eea5a2b948f5e67804600d99c9517
Famiitry <daniel.gl97@hotmail.com>
2026-03-05 21:14:29 -0500

    nuevos cambios con llaves de apis - PRUEBA 2

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Hello, Bro!");
 
 string stripe_api_key="sk_test_4eC39HqLyjwDarjT1zdp7dc";
+
+// Ejemplos de patrones que los scanners detectan
+string stripeTest = "sk_test_4eC39HqLyjwDarjT1zdp7dc"; // Stripe test key
+string githubToken = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890"; // GitHub PAT format
+string awsKey = "AKIAIOSFODNN7EXAMPLE"; // AWS example key (de su documentación oficial)
